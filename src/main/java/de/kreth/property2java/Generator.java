40314c4b9b8d7c3255af22f4284d67358d1152b5
Markus Kreth <markus.kreth@web.de>
2019-05-24 19:42:44 +0200

    Extract type to file

--- src/main/java/de/kreth/property2java/Generator.java
+++ src/main/java/de/kreth/property2java/Generator.java
@@ -84,37 +84,4 @@ public class Generator {
 		generator.start();
 	}
 
-	public class Entry {
-
-		public final String constant;
-
-		public final String key;
-
-		public final String value;
-
-		public Entry(String constant, String key, String value) {
-			super();
-			this.constant = constant;
-			this.key = key;
-			this.value = value;
-		}
-
-		public String getConstant() {
-			return constant;
-		}
-
-		public String getKey() {
-			return key;
-		}
-
-		public String getValue() {
-			return value;
-		}
-
-		@Override
-		public String toString() {
-			return "Entry [constant=" + constant + ", key=" + key + ", value=" + value + "]";
-		}
-
-	}
 }
